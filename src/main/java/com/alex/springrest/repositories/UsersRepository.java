9e79888f8d8e5b2fac3995d11670bb02495f33d9
Alex Ostapiuk <alexostapiuk00@gmail.com>
2020-06-28 09:13:32 +0300

    fix issues with queries

--- src/main/java/com/alex/springrest/repositories/UsersRepository.java
+++ src/main/java/com/alex/springrest/repositories/UsersRepository.java
@@ -3,13 +3,11 @@ package com.alex.springrest.repositories;
 import com.alex.springrest.entities.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -27,12 +25,6 @@ public interface UsersRepository extends PagingAndSortingRepository<UserEntity, 
     )
     Page<UserEntity> findAllUsersWithConfirmedEmail(Pageable pageable);
 
-    @Query(value = "select user from UserEntity user where user.firstName = :firstName")
+    @Query(value = "select user from users user where user.firstName = :firstName")
     List<UserEntity> findAllUsersWithFirstName(@Param("firstName") String firstName);
-
-    @Modifying
-    @Transactional
-    @Query(value = "update UserEntity user set user.emailVerificationStatus = :status where user.userId = :userId")
-    void updateUserVerificationStatus(@Param("status") boolean status, @Param("userId") String userId);
-
 }
